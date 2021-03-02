bfc39a2afd670c533f6f31bf23b6f93749c03366
wkk0407 <blueseakr24@gmail.com>
2021-03-02 11:36:26 -0800

    slightly reduced noise in motion detection

--- motiondetection/motion_detector.py
+++ motiondetection/motion_detector.py
@@ -15,18 +15,41 @@ sizeScore = 0
 count = 0
 
 
-fgbg = cv2.createBackgroundSubtractorMOG2()
+#fgbg = cv2.createBackgroundSubtractorMOG2()
+#kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+
+firstFrame = None
+
 while True:
     ret, frame = cap.read()
+
     if frame is None:
         break
-    
-    fgmask = fgbg.apply(frame)
+        
+    #fgmask = fgbg.apply(frame)
+    #fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel); 
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+	# if the first frame is None, initialize it
+    if firstFrame is None:
+	    firstFrame = gray
+	    continue
+
+    # compute the absolute difference between the current frame and
+	# first frame
+    frameDelta = cv2.absdiff(firstFrame, gray)
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+	# dilate the thresholded image to fill in holes, then find contours
+	# on thresholded image
+    thresh = cv2.dilate(thresh, None, iterations=2)
+
+
     count += 1
     frameScore = 0
     numObject = 0
 
-    (contours, hierarchy) = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    (contours, hierarchy) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     for c in contours:
             if cv2.contourArea(c) < 500:
                 continue
@@ -36,15 +59,20 @@ while True:
             numObject += 1
 
             (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.drawContours(frame, [c], -1, (36,255,12), 2)
 
     if (numObject != 0):
         frameScore /= numObject
     
     sizeScore += frameScore
 
+    #firstFrame = frame
+
     cv2.imshow('Frame', frame)
-    cv2.imshow('FG MASK Frame', fgmask)
+    cv2.imshow('FG MASK Frame', thresh)
+    cv2.imshow('FG MASK delta', frameDelta)
+
 
     keyboard = cv2.waitKey(20)
     if keyboard == ord("q"):
